30dc4514dfd27b9173d277c5c266204945f781d5
wbrunette <devnull@localhost>
2013-01-01 17:21:35 -0800

    apply changes from first trip

--- NoninUSBXpodPulseOx/src/org/opendatakit/sensors/drivers/xpodpulseox/PulseOxApplicationActivity.java
+++ NoninUSBXpodPulseOx/src/org/opendatakit/sensors/drivers/xpodpulseox/PulseOxApplicationActivity.java
@@ -19,12 +19,23 @@ import java.util.List;
 
 import org.opendatakit.sensors.service.BaseActivity;
 
+import com.androidplot.ui.AnchorPosition;
+import com.androidplot.ui.widget.Widget;
+import com.androidplot.xy.BoundaryMode;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XLayoutStyle;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.YLayoutStyle;
+
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.RemoteException;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -35,6 +46,8 @@ public class PulseOxApplicationActivity extends BaseActivity {
 	// Tag for Logging
 	private static final String TAG = "PulseOxApp";
 	private static final String OX_SENSOR_ID_STR = "tempSensorID";
+	
+	private static final int MAX_DATAPOINTS = 2000;
 
 	private String pulseOxId;
 
@@ -43,11 +56,19 @@ public class PulseOxApplicationActivity extends BaseActivity {
 
 	private Integer mAnswerOx;
 	private Integer mAnswerPulse;
+	private Vibrator vibrator;
 
 	private DataProcessor pulseOxProcessor;
 
 	private boolean isConnected;
 
+	//plots for plotting the data from the oxygen sensor
+	private SimpleXYSeries oxygenSeries;
+	private XYPlot dataPlot;
+	private int dataPointCounter = 0;
+	
+	private int counter = 0;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -55,7 +76,25 @@ public class PulseOxApplicationActivity extends BaseActivity {
 		setContentView(R.layout.main);
 		pulseTxt = (TextView) findViewById(R.id.pulseReading);
 		oxTxt = (TextView) findViewById(R.id.oxygenReading);
+		dataPlot = (XYPlot) findViewById (R.id.dataPlot);
+		
+ 	    Widget domainLabelWidget = dataPlot.getDomainLabelWidget();
+ 		
+        dataPlot.position(domainLabelWidget,                     // the widget to position
+                                 0,                                    // x position value, in this case 45 pixels
+                                 XLayoutStyle.ABSOLUTE_FROM_LEFT,       // how the x position value is applied, in this case from the left
+                                 0,                                     // y position value
+                                 YLayoutStyle.ABSOLUTE_FROM_BOTTOM,     // how the y position is applied, in this case from the bottom
+                                 AnchorPosition.LEFT_BOTTOM);           // point to use as the origin of the widget being positioned
 
+        dataPlot.setRangeBoundaries(87, 99, BoundaryMode.GROW);
+        // get rid of the visual aids for positioning:
+        dataPlot.disableAllMarkup();
+		
+		oxygenSeries = new SimpleXYSeries("Oxygen");
+ 		dataPlot.addSeries(oxygenSeries,new LineAndPointFormatter(Color.BLUE, Color.BLUE, Color.TRANSPARENT));
+ 		dataPointCounter = 0;
+ 		
 		// restore stored preferences if any
 		SharedPreferences appPreferences = getPreferences(MODE_PRIVATE);
 		if (appPreferences.contains(OX_SENSOR_ID_STR)) {
@@ -65,21 +104,35 @@ public class PulseOxApplicationActivity extends BaseActivity {
 			}
 		}
 
+		// get vibrator service
+		vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
 		isConnected = false;
 		mAnswerOx = 0;
 		mAnswerPulse = 0;
-		pulseOxProcessor = new DataProcessor(this);
-		pulseOxProcessor.execute();
+
 
 		Log.d(TAG, "on create");
 	}
+	
+	protected void onResume() {
+		super.onResume();
+		pulseOxProcessor = new DataProcessor(this);
+		pulseOxProcessor.execute();
+	}
+	
+    protected void onPause() {
+        super.onPause();
 
+		// stop the processor of data
+		pulseOxProcessor.cancel(true);
+    }
+    
 	@Override
 	public void onDestroy() {
 		Log.d(TAG, "onDestroy");
 
-		// stop the processor of data
-		pulseOxProcessor.cancel(true);
+
 		super.onDestroy();
 	}
 
@@ -93,8 +146,13 @@ public class PulseOxApplicationActivity extends BaseActivity {
 		}
 	}
 
+	public void recordButtonAction(View view) {
+		Log.d(TAG, "record button pressed");
+		returnValuetoCaller();
+	}
+	
 	private void connectPulseOx() {
-
+		
 		if (pulseOxId == null) {
 			Log.e(TAG, "ERROR: Somehow tried to connect when no ID is present");
 		}
@@ -103,11 +161,13 @@ public class PulseOxApplicationActivity extends BaseActivity {
 			if (!isConnected(pulseOxId)) {
 				Log.d(TAG, "connecting to sensor: " + pulseOxId);
 				sensorConnect(pulseOxId, false);
+				pulseTxt.setText("IN CONNECTING");
 			}
 			if (isConnected(pulseOxId)) {
 				Log.d(TAG, "starting pulse ox sensor: " + pulseOxId);
 				isConnected = true;
 				startSensor(pulseOxId);
+				pulseTxt.setText("IN STARTING");
 			} else {
 				Log.d(TAG, "Trouble in connecting to pulseOx sensor");
 			}
@@ -157,11 +217,15 @@ public class PulseOxApplicationActivity extends BaseActivity {
 				try {
 					List<Bundle> data = getSensorData(pulseOxId, 1);
 					if (data != null) {
+						
 						for (Bundle b : data) {
 							if (b.containsKey("connected")) {
 								boolean connected = b.getBoolean("connected");
-								if (!connected)
+								
+								if (!connected) {
+									pulseTxt.setText("DEVICE READS NOT CONNECTED");
 									continue;
+								}
 							}
 							if (b.containsKey("usable")) {
 								boolean usable = b.getBoolean("usable");
@@ -171,17 +235,35 @@ public class PulseOxApplicationActivity extends BaseActivity {
 								} else {
 									pulseTxt.setTextColor(Color.GREEN);
 									oxTxt.setTextColor(Color.BLUE);
+									int ox = b.getInt("ox");
+	    							oxygenSeries.addLast(dataPointCounter, ox);
+	    			    			dataPointCounter++;
+	    			    			dataPlot.redraw();
+	    							if(dataPointCounter > MAX_DATAPOINTS) { 
+	    								oxygenSeries.removeFirst();
+	    							}
+
 								}
+	    						
 							}
+								
 							if (b.containsKey("pulse")) {
 								int pulse = b.getInt("pulse");
-								pulseTxt.setText(Integer.toString(pulse));
+								if(pulse == 511) {
+									pulseTxt.setText("Error");
+								} else {
+									pulseTxt.setText(Integer.toString(pulse));
+								}
 								mAnswerPulse = pulse;
 								Log.d(TAG, "Got new pulse: " + pulse);
 							}
 							if (b.containsKey("ox")) {
 								int ox = b.getInt("ox");
-								oxTxt.setText(Integer.toString(ox));
+								if(ox == 127) {
+									oxTxt.setText("Error");
+								} else {
+									oxTxt.setText(Integer.toString(ox));
+								}
 								mAnswerOx = ox;
 								//vibrator.vibrate(75);
 								Log.d(TAG, "Got new oxygen: " + ox);
@@ -190,6 +272,7 @@ public class PulseOxApplicationActivity extends BaseActivity {
 
 					}
 				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
@@ -200,15 +283,19 @@ public class PulseOxApplicationActivity extends BaseActivity {
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		switch (keyCode) {
 		case KeyEvent.KEYCODE_BACK:
-			Intent intent = new Intent();
-			intent.putExtra(NoninPacket.OX, mAnswerOx);
-			intent.putExtra(NoninPacket.PULSE, mAnswerPulse);
-			setResult(RESULT_OK, intent);
-			finish();
+			returnValuetoCaller();
 			break;
 		}
 
 		return super.onKeyDown(keyCode, event);
+	}
+
+	private void returnValuetoCaller() {
+		Intent intent = new Intent();
+		intent.putExtra(NoninPacket.OX, mAnswerOx);
+		intent.putExtra(NoninPacket.PULSE, mAnswerPulse);
+		setResult(RESULT_OK, intent);
+		finish();
 	}
 
 	private class DataProcessor extends AsyncTask<Void, Void, Void> {
