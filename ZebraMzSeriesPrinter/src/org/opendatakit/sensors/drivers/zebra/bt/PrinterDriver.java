5ca057072a901f7ec9daeb080f30b29eea7eb077
mitchellsundt <mitchellsundt@gmail.com>
2013-04-01 11:25:25 -0700

    Move qrCode to top of label. Improve qrCode height computation (make it even more conservative).

--- ZebraMzSeriesPrinter/src/org/opendatakit/sensors/drivers/zebra/bt/PrinterDriver.java
+++ ZebraMzSeriesPrinter/src/org/opendatakit/sensors/drivers/zebra/bt/PrinterDriver.java
@@ -40,6 +40,7 @@ public class PrinterDriver extends AbstractDriverBaseV2  {
 
 	private static final int BARCODE_HEIGHT_DPI = 50;
 	private static final int TEXT_HEIGHT_DPI = 25;
+	private static final int QRCODE_MODULE_DPI = 6;
 
 	public PrinterDriver() {
 		Log.d(TAG," constructed.");
@@ -66,7 +67,28 @@ public class PrinterDriver extends AbstractDriverBaseV2  {
 			return null;
 		}
 
-		StringBuilder printCmd = new StringBuilder();
+		int qrCodeHeight = 0;
+		if (qrcode != null && qrcode.length() > 0) {
+
+			// for Medium ECC, Alphanumeric
+			int[] capacity = {
+					 20,  38,  61,  90, 122, 154, 178, 221, 262, 311,
+					366, 419, 483, 528, 600, 656, 734, 816, 909, 970,
+					1035, 1134, 1248, 1326, 1451, 1542, 1637, 1732, 1839, 1994,
+					2113, 2238, 2369, 2506, 2632, 2780, 2894, 3054, 3220, 3391 };
+
+			// assume 20% overhead in string (conservative?)
+			int storage = qrcode.length() + (qrcode.length()/5);
+			int level;
+			for (level = 0; level < capacity.length ; ++level ) {
+				if ( capacity[level] > storage ) {
+					break;
+				}
+			}
+			int modules = 21 + 4*(level+1);
+
+			qrCodeHeight = modules*QRCODE_MODULE_DPI + TEXT_HEIGHT_DPI;
+		}
 
 		if(labelHeight == 0) {
 
@@ -77,25 +99,7 @@ public class PrinterDriver extends AbstractDriverBaseV2  {
 			}
 
 			if (qrcode != null && qrcode.length() > 0) {
-
-				// for Medium ECC, Alphanumeric
-				int[] capacity = {
-						 20,  38,  61,  90, 122, 154, 178, 221, 262, 311,
-						366, 419, 483, 528, 600, 656, 734, 816, 909, 970,
-						1035, 1134, 1248, 1326, 1451, 1542, 1637, 1732, 1839, 1994,
-						2113, 2238, 2369, 2506, 2632, 2780, 2894, 3054, 3220, 3391 };
-
-				// assume 20% overhead in string (conservative)
-				int storage = qrcode.length() + (qrcode.length()/5);
-				int level;
-				for (level = 0; level < capacity.length ; ++level ) {
-					if ( capacity[level] > storage ) {
-						break;
-					}
-				}
-				int modules = 21 + 4*level;
-
-				labelHeight += modules*6;
+				labelHeight += qrCodeHeight;
 			}
 
 			if (strings != null) {
@@ -105,6 +109,7 @@ public class PrinterDriver extends AbstractDriverBaseV2  {
 			Log.d(TAG,"calculated label height: " + labelHeight);
 		}
 
+		StringBuilder printCmd = new StringBuilder();
 		printCmd.append("! 0 200 200 " + labelHeight + " 1\r\n ON-FEED IGNORE\r\n ENCODING UTF-8\r\n");
 
 		int yValue = 5;
@@ -113,18 +118,18 @@ public class PrinterDriver extends AbstractDriverBaseV2  {
 			yValue += BARCODE_HEIGHT_DPI;
 		}
 
+		if (qrcode != null && qrcode.length() > 0) {
+			printCmd.append("BARCODE QR 0 " + yValue + " M 2 U " + QRCODE_MODULE_DPI + "\r\n");
+			printCmd.append("MA,").append(qrcode).append("\r\n");
+			printCmd.append("ENDQR\r\n");
+			yValue += qrCodeHeight;
+		}
+
 		if (strings != null) {
 			for(String str : strings) {
 				printCmd.append("TEXT 7 0 0 " + yValue + " " + str +  " \r\n");
 				yValue +=TEXT_HEIGHT_DPI;
 			}
-		}
-
-		// print the QR code at the bottom to avoid calculating height.
-		if (qrcode != null && qrcode.length() > 0) {
-			printCmd.append("BARCODE QR 0 " + yValue + " M 2 U 6\r\n");
-			printCmd.append("MA,").append(qrcode).append("\r\n");
-			printCmd.append("ENDQR\r\n");
 		}
 
 		printCmd.append("PRINT \r\n");
