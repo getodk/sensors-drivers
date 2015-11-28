50214140c336e713dbc615c5c352ef6a952930d7
linl33 <linlee1997@gmail.com>
2015-11-28 11:38:46 -0800

    Fix whitespace

--- NoninUSBXpodPulseOx/noninusbxpodpulseox_app/src/main/java/org/opendatakit/sensors/drivers/xpodpulseox/NoninPacket.java
+++ NoninUSBXpodPulseOx/noninusbxpodpulseox_app/src/main/java/org/opendatakit/sensors/drivers/xpodpulseox/NoninPacket.java
@@ -22,95 +22,95 @@ import android.os.Bundle;
 
 class NoninPacket {
 
-	static final String OX = "ox";
-	static final String PULSE = "pulse";
-	static final String UNUSABLE = "usable";
-	static final String CONNECTED = "connected";
-	static final String PLETHYSMOGRAPHIC = "plethysmographic";
-	
-	static final int PACKET_SIZE = 25;
-	private NoninFrame [] frames;
-	
-	public NoninPacket(List<NoninFrame> pktFrames) throws NoninParseException {
-		if(pktFrames.size() != PACKET_SIZE) {
-			throw new NoninParseException("Packet has incorrect number of frames!");
-		}
-		
-		frames = pktFrames.toArray(new NoninFrame[PACKET_SIZE]);	
+   static final String OX = "ox";
+   static final String PULSE = "pulse";
+   static final String UNUSABLE = "usable";
+   static final String CONNECTED = "connected";
+   static final String PLETHYSMOGRAPHIC = "plethysmographic";
 
-		if(!frames[0].isFirstFrame()) {
-			throw new NoninParseException("Packet's first frame is not a first frame!");
-		}
-		
-		for(int i = 1; i < frames.length; i++) {
-			if(frames[i].isFirstFrame()) {
-				throw new NoninParseException("Got a first frame not in first position!");
-			}
-		}
-	}
-	
-	int getLastUnusedByte() {
-		return frames[PACKET_SIZE-1].getEndByteIndex();
-	}
-	
-	boolean sensorConnected() {
-		for(NoninFrame frame : frames) {
-			if(!frame.sensorConnected()) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	boolean unusableData() {
-		for(NoninFrame frame : frames) {
-			if(frame.unusableData()) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	
-	int getPluseRate() {
-		byte msb = frames[19].getFrameValue();
-		byte lsb = frames[20].getFrameValue();
-		// NOTE: the first bit of the LSB byte is 0 so shift only 7 instead of 8
-		return ((msb << 7) | (lsb)) & 0x1ff;
-	}
-	
-	int getExtendedPluseRate() {
-		byte msb = frames[21].getFrameValue();
-		byte lsb = frames[22].getFrameValue();
-		// NOTE: the first bit of the LSB byte is 0 so shift only 7 instead of 8
-		return ((msb << 7) | (lsb)) & 0x1ff;
-	}
-	
-	int getOxygenLevel() {
-		byte b = frames[8].getFrameValue();
-		return b & 0x7f;
-	}
-	
-	int getExtendedOxygenLevel() {
-		byte b = frames[16].getFrameValue();
-		return b & 0x7f;		
-	}
-	
-	int [] getPlethysmographic() {
-		int [] readings = new int[PACKET_SIZE];
-		for(int i = 0; i < PACKET_SIZE; i++) {
-			readings[i] = 0xff & frames[i].getPlethysmographic();
-		}
-		return readings;		
-	}
-	
-	Bundle getParsedDataBundle() {
-		Bundle parsedPkt = new Bundle();
-		parsedPkt.putBoolean(CONNECTED, sensorConnected());
-		parsedPkt.putBoolean(UNUSABLE, unusableData());
-		parsedPkt.putInt(PULSE, getExtendedPluseRate());
-		parsedPkt.putInt(OX, getExtendedOxygenLevel());
-		parsedPkt.putIntArray(PLETHYSMOGRAPHIC, getPlethysmographic());
-		return parsedPkt;
-	}
+   static final int PACKET_SIZE = 25;
+   private NoninFrame [] frames;
+
+   public NoninPacket(List<NoninFrame> pktFrames) throws NoninParseException {
+      if(pktFrames.size() != PACKET_SIZE) {
+         throw new NoninParseException("Packet has incorrect number of frames!");
+      }
+
+      frames = pktFrames.toArray(new NoninFrame[PACKET_SIZE]);
+
+      if(!frames[0].isFirstFrame()) {
+         throw new NoninParseException("Packet's first frame is not a first frame!");
+      }
+
+      for(int i = 1; i < frames.length; i++) {
+         if(frames[i].isFirstFrame()) {
+            throw new NoninParseException("Got a first frame not in first position!");
+         }
+      }
+   }
+
+   int getLastUnusedByte() {
+      return frames[PACKET_SIZE-1].getEndByteIndex();
+   }
+
+   boolean sensorConnected() {
+      for(NoninFrame frame : frames) {
+         if(!frame.sensorConnected()) {
+            return false;
+         }
+      }
+      return true;
+   }
+
+   boolean unusableData() {
+      for(NoninFrame frame : frames) {
+         if(frame.unusableData()) {
+            return true;
+         }
+      }
+      return false;
+   }
+
+
+   int getPluseRate() {
+      byte msb = frames[19].getFrameValue();
+      byte lsb = frames[20].getFrameValue();
+      // NOTE: the first bit of the LSB byte is 0 so shift only 7 instead of 8
+      return ((msb << 7) | (lsb)) & 0x1ff;
+   }
+
+   int getExtendedPluseRate() {
+      byte msb = frames[21].getFrameValue();
+      byte lsb = frames[22].getFrameValue();
+      // NOTE: the first bit of the LSB byte is 0 so shift only 7 instead of 8
+      return ((msb << 7) | (lsb)) & 0x1ff;
+   }
+
+   int getOxygenLevel() {
+      byte b = frames[8].getFrameValue();
+      return b & 0x7f;
+   }
+
+   int getExtendedOxygenLevel() {
+      byte b = frames[16].getFrameValue();
+      return b & 0x7f;
+   }
+
+   int [] getPlethysmographic() {
+      int [] readings = new int[PACKET_SIZE];
+      for(int i = 0; i < PACKET_SIZE; i++) {
+         readings[i] = 0xff & frames[i].getPlethysmographic();
+      }
+      return readings;
+   }
+
+   Bundle getParsedDataBundle() {
+      Bundle parsedPkt = new Bundle();
+      parsedPkt.putBoolean(CONNECTED, sensorConnected());
+      parsedPkt.putBoolean(UNUSABLE, unusableData());
+      parsedPkt.putInt(PULSE, getExtendedPluseRate());
+      parsedPkt.putInt(OX, getExtendedOxygenLevel());
+      parsedPkt.putIntArray(PLETHYSMOGRAPHIC, getPlethysmographic());
+      return parsedPkt;
+   }
 }
--- NoninUSBXpodPulseOx/noninusbxpodpulseox_app/src/main/java/org/opendatakit/sensors/drivers/xpodpulseox/NoninXpodPulseOx.java
+++ NoninUSBXpodPulseOx/noninusbxpodpulseox_app/src/main/java/org/opendatakit/sensors/drivers/xpodpulseox/NoninXpodPulseOx.java
@@ -28,130 +28,130 @@ import android.util.Log;
 
 public class NoninXpodPulseOx extends AbstractDriverBaseV2 {
 
-	private static final String TAG = "XpodPulseOxSensor";
+   private static final String TAG = "XpodPulseOxSensor";
 
-	public NoninXpodPulseOx() {
-		super();
-		Log.e(TAG, "Nonin Xpod PulseOx Sensor Driver constructed");
-		sensorParams.add(new SensorParameter(NoninPacket.CONNECTED, SensorParameter.Type.BOOLEAN, SensorParameter.Purpose.DATA, "is PulseOx Sensor Connected"));
-		sensorParams.add(new SensorParameter(NoninPacket.UNUSABLE, SensorParameter.Type.BOOLEAN, SensorParameter.Purpose.DATA, "is PulseOx Sensor data usable (good signals)"));
-		sensorParams.add(new SensorParameter(NoninPacket.PULSE, SensorParameter.Type.INTEGER, SensorParameter.Purpose.DATA, "Pulse"));
-		sensorParams.add(new SensorParameter(NoninPacket.OX, SensorParameter.Type.INTEGER, SensorParameter.Purpose.DATA, "Blood Oxygen Level"));
-	}
+   public NoninXpodPulseOx() {
+      super();
+      Log.e(TAG, "Nonin Xpod PulseOx Sensor Driver constructed");
+      sensorParams.add(new SensorParameter(NoninPacket.CONNECTED, SensorParameter.Type.BOOLEAN, SensorParameter.Purpose.DATA, "is PulseOx Sensor Connected"));
+      sensorParams.add(new SensorParameter(NoninPacket.UNUSABLE, SensorParameter.Type.BOOLEAN, SensorParameter.Purpose.DATA, "is PulseOx Sensor data usable (good signals)"));
+      sensorParams.add(new SensorParameter(NoninPacket.PULSE, SensorParameter.Type.INTEGER, SensorParameter.Purpose.DATA, "Pulse"));
+      sensorParams.add(new SensorParameter(NoninPacket.OX, SensorParameter.Type.INTEGER, SensorParameter.Purpose.DATA, "Blood Oxygen Level"));
+   }
 
-	@Override
-	public SensorDataParseResponse getSensorData(long maxNumReadings,
-			List<SensorDataPacket> rawSensorData, byte[] remainingData) {
+   @Override
+   public SensorDataParseResponse getSensorData(long maxNumReadings,
+       List<SensorDataPacket> rawSensorData, byte[] remainingData) {
 
-		Log.i(TAG, "Starting to parse data");
-		List<Bundle> allData = new ArrayList<Bundle>();
-		List<Byte> dataBuffer = new ArrayList<Byte>();
+      Log.i(TAG, "Starting to parse data");
+      List<Bundle> allData = new ArrayList<Bundle>();
+      List<Byte> dataBuffer = new ArrayList<Byte>();
 
-		// Copy over the remaining bytes
-		if (remainingData != null) {
-			for (Byte b : remainingData) {
-				dataBuffer.add(b);
-			}
-		}
+      // Copy over the remaining bytes
+      if (remainingData != null) {
+         for (Byte b : remainingData) {
+            dataBuffer.add(b);
+         }
+      }
 
-		// Add the new raw data
-		for (SensorDataPacket pkt : rawSensorData) {
-			byte[] payload = pkt.getPayload();
-			for (int i = 0; i < payload.length; i++) {
-				dataBuffer.add(payload[i]);
-			}
-		}
+      // Add the new raw data
+      for (SensorDataPacket pkt : rawSensorData) {
+         byte[] payload = pkt.getPayload();
+         for (int i = 0; i < payload.length; i++) {
+            dataBuffer.add(payload[i]);
+         }
+      }
 
-		List<NoninFrame> frames = attemptToCreateFrames(dataBuffer);
-		List<NoninPacket> packets = attemptToCreatePackets(frames);
+      List<NoninFrame> frames = attemptToCreateFrames(dataBuffer);
+      List<NoninPacket> packets = attemptToCreatePackets(frames);
 
-		int indexUnusedByte2 = -1;
-		for (NoninPacket pkt : packets) {
-			if (indexUnusedByte2 < pkt.getLastUnusedByte()) {
-				indexUnusedByte2 = pkt.getLastUnusedByte();
-				Log.e(TAG,
-						"INDEX UNUSED BYTE: "
-								+ Integer.toString(indexUnusedByte2));
-			}
-			allData.add(pkt.getParsedDataBundle());
-		}
+      int indexUnusedByte2 = -1;
+      for (NoninPacket pkt : packets) {
+         if (indexUnusedByte2 < pkt.getLastUnusedByte()) {
+            indexUnusedByte2 = pkt.getLastUnusedByte();
+            Log.e(TAG,
+                "INDEX UNUSED BYTE: "
+                    + Integer.toString(indexUnusedByte2));
+         }
+         allData.add(pkt.getParsedDataBundle());
+      }
 
-		// remove used bytes
-		if (indexUnusedByte2 > 0) {
-			dataBuffer.subList(0, indexUnusedByte2).clear();
-		}
+      // remove used bytes
+      if (indexUnusedByte2 > 0) {
+         dataBuffer.subList(0, indexUnusedByte2).clear();
+      }
 
-		// Copy data back into remaining buffer
-		byte[] newRemainingData = new byte[dataBuffer.size()];
-		for (int i = 0; i < dataBuffer.size(); i++) {
-			newRemainingData[i] = dataBuffer.get(i);
-		}
+      // Copy data back into remaining buffer
+      byte[] newRemainingData = new byte[dataBuffer.size()];
+      for (int i = 0; i < dataBuffer.size(); i++) {
+         newRemainingData[i] = dataBuffer.get(i);
+      }
 
-		Log.i(TAG, "Finished parsing data");
+      Log.i(TAG, "Finished parsing data");
 
-		return new SensorDataParseResponse(allData, newRemainingData);
-	}
+      return new SensorDataParseResponse(allData, newRemainingData);
+   }
 
-	private List<NoninFrame> attemptToCreateFrames(List<Byte> bytes) {
+   private List<NoninFrame> attemptToCreateFrames(List<Byte> bytes) {
 
-		List<NoninFrame> frames = new ArrayList<NoninFrame>();
+      List<NoninFrame> frames = new ArrayList<NoninFrame>();
 
-		if (bytes.size() < NoninFrame.FRAME_SIZE * 2) {
-			return frames; // not enough bytes to make frames
-		}
+      if (bytes.size() < NoninFrame.FRAME_SIZE * 2) {
+         return frames; // not enough bytes to make frames
+      }
 
-		for (int i = 0; i < bytes.size() - NoninFrame.FRAME_SIZE; i++) {
-			int exclusiveEndByte = i + NoninFrame.FRAME_SIZE;
-			if ((bytes.get(i).byteValue() == NoninFrame.START_BYTE)
-					&& (bytes.get(exclusiveEndByte).byteValue() == NoninFrame.START_BYTE)) {
+      for (int i = 0; i < bytes.size() - NoninFrame.FRAME_SIZE; i++) {
+         int exclusiveEndByte = i + NoninFrame.FRAME_SIZE;
+         if ((bytes.get(i).byteValue() == NoninFrame.START_BYTE)
+             && (bytes.get(exclusiveEndByte).byteValue() == NoninFrame.START_BYTE)) {
 
-				List<Byte> byteList = bytes.subList(i, exclusiveEndByte);
+            List<Byte> byteList = bytes.subList(i, exclusiveEndByte);
 
-				try {
-					NoninFrame frame = new NoninFrame(byteList,
-							exclusiveEndByte);
-					if (frame != null) {
-						frames.add(frame);
-						i = exclusiveEndByte - 1;
-					}
-				} catch (NoninParseException e) {
-					// failed parse move on to the set of bytes to make a frame
-					Log.w(TAG, e.getMessage());
-				}
-			}
-		}
+            try {
+               NoninFrame frame = new NoninFrame(byteList,
+                   exclusiveEndByte);
+               if (frame != null) {
+                  frames.add(frame);
+                  i = exclusiveEndByte - 1;
+               }
+            } catch (NoninParseException e) {
+               // failed parse move on to the set of bytes to make a frame
+               Log.w(TAG, e.getMessage());
+            }
+         }
+      }
 
-		return frames;
-	}
+      return frames;
+   }
 
-	private List<NoninPacket> attemptToCreatePackets(List<NoninFrame> frames) {
-		List<NoninPacket> packets = new ArrayList<NoninPacket>();
-		if (frames.size() < NoninPacket.PACKET_SIZE * 2) {
-			return packets; // not enough frames to make packets
-		}
+   private List<NoninPacket> attemptToCreatePackets(List<NoninFrame> frames) {
+      List<NoninPacket> packets = new ArrayList<NoninPacket>();
+      if (frames.size() < NoninPacket.PACKET_SIZE * 2) {
+         return packets; // not enough frames to make packets
+      }
 
 
-		for (int i = 0; i < frames.size() - NoninPacket.PACKET_SIZE; i++) {
-			int exclusiveEndFrame = i + NoninPacket.PACKET_SIZE;
-			if (frames.get(i).isFirstFrame()
-					&& frames.get(exclusiveEndFrame).isFirstFrame()) {
-				List<NoninFrame> frameList = frames.subList(i,
-						exclusiveEndFrame);
-				try {
-					NoninPacket packet = new NoninPacket(frameList);
-					if (packet != null) {
-						packets.add(packet);
-						i = exclusiveEndFrame - 1;
-					}
-				} catch (NoninParseException e) {
-					// failed parse move on to the set of frames to make a
-					// packet
-					Log.w(TAG, e.getMessage());
-				}
-			}
-		}
+      for (int i = 0; i < frames.size() - NoninPacket.PACKET_SIZE; i++) {
+         int exclusiveEndFrame = i + NoninPacket.PACKET_SIZE;
+         if (frames.get(i).isFirstFrame()
+             && frames.get(exclusiveEndFrame).isFirstFrame()) {
+            List<NoninFrame> frameList = frames.subList(i,
+                exclusiveEndFrame);
+            try {
+               NoninPacket packet = new NoninPacket(frameList);
+               if (packet != null) {
+                  packets.add(packet);
+                  i = exclusiveEndFrame - 1;
+               }
+            } catch (NoninParseException e) {
+               // failed parse move on to the set of frames to make a
+               // packet
+               Log.w(TAG, e.getMessage());
+            }
+         }
+      }
 
-		return packets;
-	}
+      return packets;
+   }
 
 }
